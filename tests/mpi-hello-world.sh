3bab20eb7d0dd70a55d2bd77436344913e7ef0f8
Gregory Price <gregory@gregoryprice.net>
2017-09-25 22:09:31 -0400

    updating OS push

---
+++ tests/mpi-hello-world.sh
@@ -0,0 +1,4 @@
+dmtcp_launch --with-plugin ../plugin/mpi_tutorials/libdmtcp_mpi_tutorials.so mpirun ../mpitutorial/tutorials/mpi-hello-world/code/mpi_hello_world
+#sleep 1
+#dmtcp_command -c
+#dmtcp_command -k
